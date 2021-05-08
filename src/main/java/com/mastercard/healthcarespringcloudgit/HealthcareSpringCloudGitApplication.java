16fed45df486fa66808117d3b00aa8456bb0b825
ganesh <pawarganesha1990@gmail.com>
2021-05-08 20:33:06 +0530

    This is first commit for spring cloud config server with git

---
+++ src/main/java/com/mastercard/healthcarespringcloudgit/HealthcareSpringCloudGitApplication.java
@@ -0,0 +1,13 @@
+package com.mastercard.healthcarespringcloudgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareSpringCloudGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareSpringCloudGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastercard/healthcarespringcloudgit/HealthcareSpringCloudGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercard.healthcarespringcloudgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareSpringCloudGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
