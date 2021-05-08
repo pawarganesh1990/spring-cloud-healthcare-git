50c46155f0020bbd33e59c44338a1ac4a66bd3d2
ganesh <pawarganesha1990@gmail.com>
2021-05-08 20:44:04 +0530

    property files with rate info according to profile

--- src/main/java/com/mastercard/healthcarespringcloudgit/HealthcareSpringCloudGitApplication.java
+++ src/main/java/com/mastercard/healthcarespringcloudgit/HealthcareSpringCloudGitApplication.java
@@ -2,8 +2,10 @@ package com.mastercard.healthcarespringcloudgit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HealthcareSpringCloudGitApplication {
 
 	public static void main(String[] args) {
